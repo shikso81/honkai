3fdfaed149c2ddca58f8b932567279eb0c2c6651
shikso81 <nj.company.777@gmail.com>
2024-08-01 02:11:56 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,208 @@
+from fake_useragent import UserAgent
+import requests
+import random
+from termcolor import colored
+import pyfiglet
+import os
+from banner import banner
+import subprocess
+import sys
+import webbrowser
+import json
+import urllib.request
+import requests
+from bs4 import BeautifulSoup
+import phonenumbers
+from phonenumbers import geocoder, carrier, timezone
+import whois
+import colorama
+from colorama import Fore, init
+from pystyle import Anime, Colors, Colorate, Center
+import requests
+import socket
+import getpass
+import datetime
+
+color_code = {
+    "reset": "\033[0m",  
+    "underline": "\033[04m", 
+    "green": "\033[32m",     
+    "yellow": "\033[93m",    
+    "red": "\033[31m",       
+    "cyan": "\033[36m",     
+    "bold": "\033[01m",        
+    "pink": "\033[95m",
+    "url_l": "\033[36m",       
+    "li_g": "\033[92m",      
+    "f_cl": "\033[0m",
+    "dark": "\033[90m",     
+}
+os.system("clear")
+os.system("cls")
+
+alignment = "{:>50}".format(banner)
+colored_banner = Colorate.Horizontal(Colors.white_to_red, alignment)
+print(colored_banner)
+select = input(f'{color_code["cyan"]}[:D]{color_code["bold"]} select >{color_code["yellow"]} ')
+if select == '1':
+    subprocess.run(['python', '/storage/emulated/0/honkai/Laitoxx BY @asoruperehod/laitoxx.py'])
+
+
+if select == '2':
+    subprocess.run(['python', '/storage/emulated/0/honkai/ip-main/ip.py'])
+    
+if select == '3':
+    subprocess.run(['python', 'interia.py'])
+    
+if select == '4':
+  subprocess.run(['python', 'snoser.py'])
+
+elif select == '5':
+    subprocess.run(['python', 'rockefeller.py'])
+    
+elif select == '6':
+    subprocess.run(['python', '/storage/emulated/0/honkai/satoshi/main.py'])
+    
+elif select == '7':
+    subprocess.run(['python', '/storage/emulated/0/honkai/anubis.py'])
+
+elif select == '8':
+    subprocess.run(['python', '/storage/emulated/0/honkai/NatHack/main.py'])
+
+elif select == '9':
+    subprocess.run(['python', 'mrpenis.py'])
+
+elif select == '10':
+   subprocess.run(['python', '/storage/emulated/0/honkai/espada/main.py'])
+
+elif select == '11':
+   subprocess.run(['python', '/storage/emulated/0/honkai/noblack-mail-main/noblack-mail.py'])
+
+elif select == '12':
+  webbrowser.open('http://zefoy.com')
+
+elif select == '13':
+  subprocess.run(['python', 'crazysnos.py'])
+
+elif select == '14':
+  subprocess.run(['python', 'crazytool.py'])
+
+elif select == '15':
+  subprocess.run(['python', '/storage/emulated/0/honkai/Raptor-Multi-Tool-main/Raptor.py'])
+
+elif select == '16':
+  subprocess.run(['python', 'tgphish.py'])
+
+elif select == '17':
+   subprocess.run(['python', 'builder.py'])
+
+elif select == '18':
+   subprocess.run(['python', 'builder.py'])
+
+elif select == '19':
+   subprocess.run(['python', 'builder.py'])
+
+elif select == '20':
+   subprocess.run(['python', 'builder.py'])
+
+elif select == '21':
+   subprocess.run(['python', 'builder.py'])
+
+elif select == '22':
+   subprocess.run(['python', 'builder.py'])
+
+elif select == '23':
+   subprocess.run(['python', 'builder.py'])
+
+elif select == '24':
+   subprocess.run(['python', 'builder.py'])
+
+elif select == '25':
+   subprocess.run(['python', 'builder.py'])
+
+elif select == '29':
+   subprocess.run(['python', 'builder.py'])
+
+elif select == '34':
+   subprocess.run(['python', 'fio.py'])
+
+elif select == '33':
+   subprocess.run(['python', 'nick.py'])
+
+elif select == '32':
+   subprocess.run(['python', 'ip.py'])
+
+
+elif select == '31':
+   subprocess.run(['python', 'number.py'])
+
+elif select == '35':
+   subprocess.run(['python', 'troll.py'])
+
+elif select == '36':
+   subprocess.run(['python', 'name.py'])
+
+elif select == '37':
+  subprocess.run(['python', 'ukphone.py'])
+
+elif select == '38':
+  subprocess.run(['python', 'swat.py'])
+
+elif select == '39':
+  subprocess.run(['python', 'genip.py'])
+
+elif select == '40':
+    subprocess.run(['python', 'mullvad.py'])
+
+elif select == '41':
+  subprocess.run(['python', 'genmac.py'])
+
+elif select == '42':
+  subprocess.run(['python', 'swat.py'])
+
+elif select == '44':
+  subprocess.run(['python', 'pars.py'])
+
+elif select == '45':
+    subprocess.run(['python', 'trollq.py'])
+
+elif select == '46':
+   print(colored(f"1. ошибка: No module named [название модуля] фикс : pip install [название модуля] 2. ошибка: No module named PIL фикс : pip install pillow 3. ошибка: error: Permission decided фикс : команда termux-setup-storage , потом выдаем разрешение на файлы и медиафайлы.", 'white'))
+
+
+
+elif select == '47':
+   subprocess.run(['python', 'manualsnoc.py'])
+
+elif select == '48':
+   subprocess.run(['python', 'manualhz.py'])
+
+elif select == '49':
+     subprocess.run(['python', 'manualfree.py'])
+
+elif select == '50': 
+   subprocess.run(['python', '20kproxy.py'])
+
+elif select == '51':
+   subprocess.run(['python', 'manualdox.py'])
+
+elif select == '52':
+    subprocess.run(['python', 'allmanual.py'])
+
+elif select == '53':
+  subprocess.run(['python', 'manualswat.py'])
+
+elif select == '54':
+   subprocess.run(['python', 'manualdoxx.py'])
+
+elif select == '55':
+   subprocess.run(['python', 'manualdoxxx.py'])
+
+elif select == '56':
+   subprocess.run(['python', 'verman.py'])
+
+elif select == '57':
+   subprocess.run(['python', 'doxvirts.py'])
+
+elif select == '58':
+   subprocess.run(['python', 'sssmanual.py'])
---
+++ manualdox.py
@@ -0,0 +1,82 @@
+import os
+doxingg = """третий апдейт, добавлено побольше ботов, а так же сайтов.
+
+писал m9guqrd
+
+bots
+
+@QuickOSINT_bot
+├ @clerkinfobot
+├ @find_caller_bot
+├ @get_caller_bot
+├ @get_kolesa_bot
+├ @get_kontakt_bot
+├ @getbank_bot
+├ @GetFb_bot
+├ @Getphonetestbot
+├ @info_baza_bot
+├ @mailsearchbot
+├ @MyGenisBot
+├ @phone_avito_bot
+├ @SafeCallsBot
+└ @usersbox_bot могу сказать одну вещь, БОТ ИМБА НАХУЙ находит дохуя инфы
+@UniversalSearchBot
+@StealDetectorBOT
+@last4mailbot
+@info_baza_bot
+@get_caller_bot
+@dosie_Bot
+@eyeofbeholder_bot
+@clerkinfobot
+@TeleSINT_Bot
+@fuckeddb
+@freedomf0x
+@usinfobot
+https://saverudata.online/
+https://r23.fssp.gov.ru/iss/ip
+https://legalacts.ru/
+https://www.nalog.gov.ru/
+@kolibri_osint_bot
+@Qsta_bot
+@clerkinfobot
+@clerkinfobot
+@get_caller_bot
+@Getphonetestbot
+http://avinfo.guru
+http://getcontact.com
+http://mirror.bullshit.agency
+http://numberway.com
+http://mirror.bullshit.agency
+http://mysmsbox.ru
+http://phoneInfoga.crvx.fr
+http://spravnik.com
+http://teatmik.ee
+http://truecaller.com
+
+
+ахуенные инструменты для докса
+
+www.whois.net
+
+www.pipl.com
+
+www.tineye.com
+
+www.geobytes.com/iplocator.html
+
+www.whitepages.com
+
+www.118.com
+
+www.knowem.com
+
+http://www.spokeo.com/username-search
+
+http://www.zabasearch.com/advanced.php
+
+http://www.ip-address.org/lookup/ip-locator.php
+
+skypegrab.net
+"""
+
+print(doxingg)
